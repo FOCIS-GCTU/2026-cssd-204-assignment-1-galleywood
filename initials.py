1d0d96f9a2bcab45a99216ae11e9fd4612f5f2ca
GALLEY WISDOM <137475831+galleywood@users.noreply.github.com>
2026-05-31 09:14:25 +0000

    Submit assignment 1

--- initials.py
+++ initials.py
@@ -1,6 +1,38 @@
+# File: initials.py
+# Description: Prints my initials in stylized large block letters
+# Assignment Number: 1
+#
+# Name: WISDOM GALLEY KODZO
+# Student ID: 2425400731
+# Email: 2425400731@live.gctu.edu.gh
+# Grader: Augustus
+
+# On my honor, Galley Wisdom, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """This function handles the formatted printing of my initials."""
+    # Starting blank line
+    print()
+
+    # Small initials
+    print('...GWK')
+
+    # Blank line after small initials
+    print()
+
+    # Large initials
+    print('....GGGGGGGGGG.........WWWW....WWWW........KKKK....KKKK...')
+    print('...GGGGGGGGGGGG........WWWW....WWWW........KKKK...KKKK....')
+    print('...GGGG.....GGG........WWWW....WWWW........KKKK..KKKK.....')
+    print('...GGGG................WWWW....WWWW........KKKKKKKK.......')
+    print('...GGGG...GGGGG........WWWW.WW.WWWW........KKKKKKK........')
+    print('...GGGG......GG........WWWWWWWWWWWW........KKKKKKKK.......')
+    print('...GGGG......GG........WWWWWWWWWWWW........KKKK.KKKK.....')
+    print('...GGGGGGGGGGGG........WWWW....WWWW........KKKK..KKKK....')
+    print('...GGGGGGGGGGGG...**...WWWW....WWWW...**...KKKK...KKKK...')
+    print('....GGGGGGGGGG....**...WWWW....WWWW...**...KKKK....KKKK...')
 
 
 
